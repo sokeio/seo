a5c6955d9f55c2c17e02e8b1fcc9282cee181efb
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-10-20 18:06:45 +0700

    update

---
+++ config/seo.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => 'Seo',
+    'commands' => [],
+    'fields' => [],
+    'shortcodes' => [],
+    'actions' => [],
+    'widgets' => [],
+];
---
+++ src/SeoServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace BytePlatform\Seo;
+
+use Illuminate\Support\ServiceProvider;
+use BytePlatform\Laravel\ServicePackage;
+use BytePlatform\Laravel\WithServiceProvider;
+
+class SeoServiceProvider extends ServiceProvider
+{
+    use WithServiceProvider;
+
+    public function configurePackage(ServicePackage $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         */
+        $package
+            ->name('seo')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasHelpers()
+            ->hasAssets()
+            ->hasTranslations()
+            ->runsMigrations();
+    }
+    public function extending()
+    {
+    }
+    public function packageRegistered()
+    {
+        $this->extending();
+    }
+    private function bootGate()
+    {
+        if (!$this->app->runningInConsole()) {
+            add_filter(PLATFORM_PERMISSION_CUSTOME, function ($prev) {
+                return [
+                    ...$prev
+                ];
+            });
+        }
+    }
+    public function packageBooted()
+    {
+        $this->bootGate();
+    }
+}
