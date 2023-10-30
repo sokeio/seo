319f1ad2732f37491113bc10792d02c4147d73b3
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-10-30 11:19:44 +0700

    Update HasSEO.php

--- src/HasSEO.php
+++ src/HasSEO.php
@@ -6,9 +6,41 @@ use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 trait HasSEO
 {
+    public function getSeoTitle()
+    {
+        return $this->name;
+    }
+    public function getSeoDescription()
+    {
+        return $this->description;
+    }
+    public function getSeoImage()
+    {
+        return $this->image;
+    }
+    public function getSeoAuthor()
+    {
+        return $this->author ?? '';
+    }
+    public function getSeoRobots()
+    {
+        return $this->robots ?? '';
+    }
+
+    public function getSeoCanonicalUrl()
+    {
+        return $this->canonical_url ?? '';
+    }
     public function addSEO(): static
     {
-        $this->seo()->create();
+        $this->seo()->create([
+            'title' => $this->getSeoTitle(),
+            'description' => $this->getSeoDescription(),
+            'image' => $this->getSeoImage(),
+            'author' => $this->getSeoAuthor(),
+            'robots' => $this->getSeoRobots(),
+            'canonical_url' => $this->getSeoCanonicalUrl()
+        ]);
 
         return $this;
     }
